a044a47c0b675c4609c30f50360d6b55b68cf1dd
Daniela hitomi <daniela.hitomi@usp.br>
2025-11-17 22:19:02 -0300

    pesquisa inicio atletas e atleticas

--- staticpages/models.py
+++ staticpages/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Atletica(models.Model):
+    nome = models.CharField(max_length=100)
+    curso = models.CharField(max_length=100, blank=True)
+    universidade = models.CharField(max_length=150, blank=True)
+    email = models.EmailField(blank=True)
+    instagram = models.CharField(max_length=100, blank=True)
+    descricao = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.nome
+
+
+class Atleta(models.Model):
+    nome = models.CharField(max_length=100)
+    esporte = models.CharField(max_length=100)
+    curso = models.CharField(max_length=100, blank=True)
+    email = models.EmailField(blank=True)
+    instagram = models.CharField(max_length=100, blank=True)
+    bio = models.TextField(blank=True)
+
+    # opcional: relacionar com uma atlética
+    atletica = models.ForeignKey(
+        Atletica,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='atletas'
+    )
+
+    def __str__(self):
+        return self.nome
