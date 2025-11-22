ef13adffd4a9d89f12d0881ef682ed2e2938c73f
Andre Calza Cassim <andreccassim@usp.br>
2025-11-21 22:56:22 -0300

    Revert "registrar funciona"

--- staticpages/urls.py
+++ staticpages/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('atleta/<int:pk>/', views.perfil_atleta, name='perfil_atleta'),
     path('atletica/<int:pk>/', views.perfil_atletica, name='perfil_atletica'),
-    path('signup/', views.signup, name='signup')
 ]
