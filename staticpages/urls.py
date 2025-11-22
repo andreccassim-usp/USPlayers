9282f45f7084b980a219129b40c47bd0de397299
Andre Calza Cassim <andreccassim@usp.br>
2025-11-21 23:01:47 -0300

    Revert "Merge branch 'Bia' into 'main'"

--- staticpages/urls.py
+++ staticpages/urls.py
@@ -6,8 +6,6 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('', views.index, name='index'),
     path('buscar/', views.buscar, name='buscar'),
-    path('comparacao/', views.comparacao, name='comparacao'),
-    path('login/', views.login, name='login'),
     path('atleta/<int:pk>/', views.perfil_atleta, name='perfil_atleta'),
     path('atletica/<int:pk>/', views.perfil_atletica, name='perfil_atletica'),
 ]
